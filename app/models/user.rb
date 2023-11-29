883dc8cd16c52073f83af348b9262de9fc9c02a9
morinonusi421 <78341654+morinonusi421@users.noreply.github.com>
2023-11-29 06:44:22 +0000

    uniq_scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   enum sex: { 男の子: 0, 女の子: 1 }
   belongs_to :party
-  validates :name,  presence: true, length: { maximum: 10 }, uniqueness: true
+  validates :name,  presence: true, length: { maximum: 10 }, uniqueness: { scope: :party_id }
 end
