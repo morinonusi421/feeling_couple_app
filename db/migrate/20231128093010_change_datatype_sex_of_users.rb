d9137a6f78cd084705514e638aaca0c6caa239c4
morinonusi421 <78341654+morinonusi421@users.noreply.github.com>
2023-11-29 03:33:35 +0000

    fixmigrate2

--- db/migrate/20231128093010_change_datatype_sex_of_users.rb
+++ db/migrate/20231128093010_change_datatype_sex_of_users.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeSexOfUsers < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :sex, :integer USING CAST(sex AS integer)'
+    change_column :users, :sex, 'integer USING CAST(sex AS integer)'
   end
 end
