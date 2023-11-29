e4fcbfd9bacfa950b629e38deea62548e8419813
morinonusi421 <78341654+morinonusi421@users.noreply.github.com>
2023-11-29 02:50:26 +0000

    add layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "フィーリングカップル"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
