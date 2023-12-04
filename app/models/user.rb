f0f55b4deb239dc214648edd94853500b6ca5703
morinonusi421 <78341654+morinonusi421@users.noreply.github.com>
2023-12-04 12:11:46 +0000

    finish_choosing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 10 }, uniqueness: { scope: :party_id }
   validates :sex, presence: true
 
-  has_many :loved, class_name: "User", foreign_key: "loving_id"
+  has_many :loved, class_name: "User", foreign_key: "loving_id", dependent: :nullify
 
   belongs_to :loving, class_name: "User", optional: true, inverse_of: :loved
+  
+
 end
